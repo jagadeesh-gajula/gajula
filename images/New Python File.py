9730b113eb44e7f3ca2e989d7014e5738b6cd180
Jagadeesh Gajula <jagadeesh.jd54@gmail.com>
2020-09-18 17:31:42 +0530

    auth

---
+++ images/New Python File.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os
+
+
+def convert_quality(path,dest,quality=70,grayscale=False):
+    foo = Image.open(path)
+    if grayscale == True:
+        foo = foo.convert('L')
+    x = int(foo.size[0]*(quality/100))
+    y = int(foo.size[1]*(quality/100))
+    foo = foo.resize((x,y),Image.ANTIALIAS)
+    foo.save(dest,optimize=True,quality=quality)
+
+
+i = 'author.png'
+convert_quality(i,i,70)
